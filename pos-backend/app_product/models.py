7b21238b0f467c1271870c4efe033c0a520499ee
Hadayetullah <hadayetullah30@gmail.com>
2024-08-15 22:16:04 +0600

    Create LowStockWarning model

--- pos-backend/app_product/models.py
+++ pos-backend/app_product/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 import uuid
 
+from app_useraccount.models import User
+
 # Create your models here.
 class Category(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -38,3 +40,13 @@ class Product(models.Model):
     category = models.ForeignKey(Category, related_name='categories', on_delete=models.CASCADE)
     subcategory = models.ForeignKey(SubCategory, related_name='subcategories', on_delete=models.CASCADE)
     variant = models.ForeignKey(Variant, related_name='variants', on_delete=models.CASCADE)
+
+
+
+class LowStockWarning(models.Model):
+    is_warned = models.BooleanField(default=False)
+    description = models.TextField()
+    warned_time = models.DateTimeField(blank=True, null=True)
+    user = models.ForeignKey(User, related_name='lowstockwarning', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='product', on_delete=models.CASCADE)
+
